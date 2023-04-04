2e781fa2410dd35cf3cbf74059033280c41fb52c
mjh9401 <mjh9401@naver.com>
2023-04-04 22:28:22 +0900

    beautifulSoup 실습1

---
+++ beeutifulsoup.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+
+url = "https://www.naver.com"
+res = req.get(url)
+
+soup = bs(res.text, 'html.parser')
+print(soup.a)
+
--- 정규식.py
+++ 정규식.py
@@ -7,4 +7,4 @@ print(re.match(r'how are you\?',s))
 e = '이 영화는 F등급 입니다'
 
 print(re.match(r'이 영화는 [ABCF]등급 입니다',e))                                           
-print(re.findall(r'이 (..)는 (.)등급 입니다',e))  
+print(re.findall(r'이 (.+)는 (.*)등급 입니다',e))  
