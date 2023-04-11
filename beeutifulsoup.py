742a35f7b89237ee90d2573bb1a613ccbd8a62b5
mjh9401 <mjh9401@naver.com>
2023-04-11 10:37:44 +0900

    beautifulSoup 실습2

--- beeutifulsoup.py
+++ beeutifulsoup.py
@@ -1,9 +1,28 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
 
-url = "https://www.naver.com"
+url = "https://finance.naver.com/marketindex/exchangeList.naver"
 res = req.get(url)
 
 soup = bs(res.text, 'html.parser')
-print(soup.a)
+# print(soup.title)
+# print(soup.title.string)
+
+tds = soup.find_all('td')
+
+names = []
+for td in tds:
+    if len(td.find_all('a')) == 0:
+       continue 
+    names.append(td.get_text(strip=True))
+
+prices = []
+for td in tds:
+    if 'class' in td.attrs:
+        if 'sale' in td.attrs['class']:
+            prices.append(td.get_text(strip=True))
+    
+
+print(names)
+print(prices)
 
